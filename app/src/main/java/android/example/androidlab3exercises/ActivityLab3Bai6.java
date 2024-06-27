3d9704a2615d594847f91a70bfbad66c90972168
HowardPeter <phuocnt1611@gmail.com>
2024-06-28 01:19:26 +0700

    Bai 6 completed

--- app/src/main/java/android/example/androidlab3exercises/ActivityLab3Bai6.java
+++ app/src/main/java/android/example/androidlab3exercises/ActivityLab3Bai6.java
@@ -3,6 +3,8 @@ package android.example.androidlab3exercises;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,6 +12,7 @@ import android.widget.ListView;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class ActivityLab3Bai6 extends AppCompatActivity {
     Spinner spDanhMuc;
@@ -18,7 +21,8 @@ public class ActivityLab3Bai6 extends AppCompatActivity {
     ListView lvSp;
 
     ArrayList<String> spinnerList = new ArrayList<>();
-    ArrayList<SanPhamMobile> lvArrayList = new ArrayList<>();
+    ArrayList<SanPhamMobile> lvArrayList = new ArrayList<>(); // List hiển thị lên màn hình
+    ArrayList<SanPhamMobile> allSanPham = new ArrayList<>(); // List chứa tất cả sản phẩm
     ArrayAdapter<String> spinnerAdapter;
     ArrayAdapter<SanPhamMobile> lvAdapter;
     String[] lsDanhMuc = new String[]{"1 - Samsung", "2 - Iphone", "3 - Ipad"};
@@ -32,6 +36,8 @@ public class ActivityLab3Bai6 extends AppCompatActivity {
         addControl();
         setUpSpinner();
         setUpListView();
+        addEvent();
+        filterProductsByCategory(1);
     }
     void addControl()
     {
@@ -51,8 +57,51 @@ public class ActivityLab3Bai6 extends AppCompatActivity {
     }
     void setUpListView()
     {
-        lvArrayList = SanPhamMobile.initData(lsLoai, lsMaSp, lsTenSP);
+        allSanPham = SanPhamMobile.initData(lsLoai, lsMaSp, lsTenSP);
         lvAdapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, lvArrayList);
         lvSp.setAdapter(lvAdapter);
+    }
+    void addEvent()
+    {
+        btnThem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String danhmuc = spDanhMuc.getSelectedItem().toString().substring(0, 1); //lấy giá trị đầu của chuỗi selected spinner item
+                int loai = Integer.parseInt(danhmuc);
+                String masp = edtMaSp.getText().toString();
+                String tensp = edtTenSp.getText().toString();
+                if(!masp.isEmpty() || !tensp.isEmpty())
+                {
+                    SanPhamMobile sanPhamMobile = new SanPhamMobile(loai, masp, tensp);
+                    allSanPham.add(sanPhamMobile); // thêm vào list tất cả sản phẩm
+                }
+                filterProductsByCategory(loai);
+                lvAdapter.notifyDataSetChanged();
+
+                edtMaSp.setText("");
+                edtTenSp.setText("");
+            }
+        });
+        spDanhMuc.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // lấy giá trị số ở đầu chuỗi selected spinner item và chuyển thành int
+                int selectedCategory = Integer.parseInt(parent.getItemAtPosition(position).toString().substring(0, 1));
+                filterProductsByCategory(selectedCategory);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                lvAdapter.clear();
+                lvAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+    void filterProductsByCategory(int category) {
+        lvAdapter.clear();
+        lvAdapter.addAll(allSanPham.stream()
+                .filter(product -> product.getLoaiSp() == category)
+                .collect(Collectors.toList()));
+        lvAdapter.notifyDataSetChanged();
     }
 }
